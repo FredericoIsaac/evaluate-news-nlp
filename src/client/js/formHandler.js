3013795ea5c4c54b0e0bce5b6d7f1111c6b8cd22
Lisa Kamino <lkamino@gmail.com>
2020-03-09 13:33:49 -0700

    completed test api call

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,3 +1,6 @@
+// Personal API Key for OpenWeatherMap API
+const apiKey= 'd73f64c6babee8a19e82551dbc221640';
+
 function handleSubmit(event) {
     event.preventDefault()
 
@@ -5,12 +8,17 @@ function handleSubmit(event) {
     let formText = document.getElementById('name').value
     Client.checkForName(formText)
 
+    const newZip =  document.getElementById('zip').value;
+    const url = `http://api.openweathermap.org/data/2.5/weather?zip=${newZip},us&units=imperial&appid=${apiKey}`
+
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    fetch(url)
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+        console.log(res.main.temp)
+        document.getElementById('results').innerHTML = res.main.temp
     })
 }
 
 export { handleSubmit }
+
--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,6 @@
 var path = require('path')
 const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+// const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
 
@@ -17,6 +17,6 @@ app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
 })
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+// app.get('/test', function (req, res) {
+//     res.send(mockAPIResponse)
+// })
