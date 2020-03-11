c3897b39571c8d7cc9844ca1f001568c5c03cc43
Lisa Kamino <lkamino@gmail.com>
2020-03-11 11:56:06 -0700

    installed dev tools

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,10 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
+const TerserPlugin = require("terser-webpack-plugin")
+
 
 module.exports = {
     entry: './src/client/index.js',
@@ -8,6 +12,9 @@ module.exports = {
     output: {
         libraryTarget: 'var',
         library: 'Client'
+    },
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
     },
     module: {
         rules: [
@@ -18,7 +25,7 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
             }
         ]
     },
@@ -26,6 +33,7 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new MiniCssExtractPlugin({filename: '[name].css'})
     ]
 }
