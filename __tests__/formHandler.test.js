c8e1d78ff37d7ddb9df82c6aaae79dce01146bf5
Lisa Kamino <lkamino@gmail.com>
2020-03-16 11:40:10 -0700

    refactored file structure and added formHandler test file

---
+++ __tests__/formHandler.test.js
@@ -0,0 +1,7 @@
+import { handleSubmit } from "../src/client/js/formHandler";
+
+describe(handleSubmit, () => {
+    test("it should return 2", () => {
+        expect(1 + 1).toEqual(2);
+    });
+});
